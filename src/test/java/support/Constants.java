0056e8158b95964b3f14a9a881b99573b1628cdb
BrunoJBorges <BrunoJBorges@users.noreply.github.com>
2023-11-03 20:19:32 +0000

    Automated deployment: Fri Nov  3 20:19:32 UTC 2023 36d6b286165edd03da822c40421daf7f567f7d4d

--- src/test/java/support/Constants.java
+++
@@ -1,9 +0,0 @@
-package support;
-
-public class Constants {
-	
-	public static final String URL_PROFIS = "https://www.saucedemo.com/v1/";
-	public static final String USER_EMAIL = "standard_user";
-	public static final String USER_PASSWORD  = "secret_sauce";
-
-}
